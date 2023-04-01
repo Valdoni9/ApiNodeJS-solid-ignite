0d0699b32eacc0ff7fdfa678317f8db4034ba366
Antonio Valdoni <antoniovaldoni@gmail.com>
2023-04-01 02:13:52 -0400

    Nodejs-solid-ignite

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🪃 Http server listening on port!')
+  })
